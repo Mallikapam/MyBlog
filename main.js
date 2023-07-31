0d29264f72c2ccc956a45def808232e6406ea5dc
Mallika <pamulamallika@gmail.com>
2023-07-30 23:58:07 -0400

    dark mode

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 // javascript
-console.log("hi")
-const name = document.querySelector(".name");
+
+function darkMode() {
+    let element = document.body;
+    let content = document.getElementById("DarkModetext");
+    element.className = "dark-mode";
+
+}
+function lightMode() { 
+    let element = document.body;
+    let content = document.getElementById("DarkModetext");
+    element.className = "light-mode";
+}
+/*
 function darkMode(name) {
     //code
-    document.querySelector(".name").style.color = "blue";
+    document.querySelector(".name").style.color = "white";
+    document.querySelector(".name").style.color = "white";
+    document.querySelector(".name").style.color = "white";
+    document.querySelector(".name").style.color = "white";
+    document.querySelector(".name").style.color = "white";
+    document.querySelector(".name").style.color = "white";
+    document.querySelector(".name").style.color = "white";
+    document.querySelector(".name").style.color = "white";
 }
-let darkMode =document.getElementById("darkMode");
+*/
