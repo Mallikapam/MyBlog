32b8d378d09987ea11634bb93ca07970dd2096b0
Mallika <pamulamallika@gmail.com>
2023-07-30 22:18:12 -0400

    adding button

---
+++ main.js
@@ -0,0 +1,8 @@
+// javascript
+console.log("hi")
+const name = document.querySelector(".name");
+function darkMode(name) {
+    //code
+    document.querySelector(".name").style.color = "blue";
+}
+let darkMode =document.getElementById("darkMode");
